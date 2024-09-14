9f16b4f9a879f06572882de5c93f23cdd42fefe7
kazucch0925 <kazuto925.corocoro@hotmail.com>
2024-09-15 00:10:53 +0900

    storage resource changed

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'docuhubMinutes'
+  name: 'docuhubMinutes',
+  access: (allow) => ({
+    'minutes/*': [
+        allow.guest.to(['read']),
+        allow.authenticated.to(['read', 'write', 'delete'])
+    ]
+  })
 });
