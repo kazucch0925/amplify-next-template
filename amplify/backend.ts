994802e2baba3001d320ef33e7c37382f8c3b45d
kazucch0925 <kazuto925.corocoro@hotmail.com>
2024-09-16 00:48:25 +0900

    authType username add

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,8 +3,12 @@ import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { storage } from './storage/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage
 });
+
+const { cfnUserPool } = backend.auth.resources.cfnResources
+// an empty array denotes "email" and "phone_number" cannot be used as a username
+cfnUserPool.usernameAttributes = []
