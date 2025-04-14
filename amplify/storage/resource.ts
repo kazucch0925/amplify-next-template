fc103f72ba71a494abc55af55c5d5f9bcd59ae25
kazucch0925 <kazuto925.corocoro@hotmail.com>
2025-04-14 15:49:00 +0900

    resourse.tsのprivateとsharedの権限設定を簡素化

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,20 +3,12 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'docuhubMinutes',
   access: (allow) => ({
-    // バケット全体へのリスト操作権限を追加
-    'minutes/*': [
-      allow.authenticated.to(['list']),
-    ],
-    // プライベートフォルダのルートへのアクセス権限
-    'minutes/private/*': [
-      allow.authenticated.to(['list', 'write']),
+    // 共有フォルダへのアクセス権限
+    'minutes/shared/*': [
+      allow.authenticated.to(['get', 'write', 'delete', 'list']),
     ],
     // ユーザーごとのプライベートフォルダ
     'minutes/private/${user.sub}/*': [
-      allow.authenticated.to(['get', 'write', 'delete', 'list']),
-    ],
-    // 共有フォルダ（認証済みユーザー全員が読み取りと書き込み可能）
-    'minutes/shared/*': [
       allow.authenticated.to(['get', 'write', 'delete', 'list']),
     ]
   })
